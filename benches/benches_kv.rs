7de829d67fffb5c5fd50648c4d7dbe2508b3a350
Siddharth Singh <me@shantaram.xyz>
2025-05-04 12:20:17 +0530

    add benchmarks

---
+++ benches/benches_kv.rs
@@ -0,0 +1,48 @@
+#![feature(test)]
+
+extern crate test;
+
+#[cfg(feature = "sqlite")]
+use stupid_simple_kv::storages::sqlite_backend::SqliteBackend;
+use stupid_simple_kv::{Kv, MemoryBackend, key};
+
+#[cfg(test)]
+mod benches {
+    use super::*;
+    use test::{Bencher, black_box};
+
+    #[bench]
+    fn bench_memory_set_get(b: &mut Bencher) {
+        let mut kv = Kv::new(MemoryBackend::new());
+        b.iter(|| {
+            for i in 0..1000u64 {
+                let k = key!["num", i];
+                kv.set(k.clone(), black_box(i)).unwrap();
+                let _ = kv.get::<u64>(&k).unwrap();
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_key_encoding(b: &mut Bencher) {
+        b.iter(|| {
+            for i in 0u64..10000 {
+                black_box(key!["foo", i, "bar", true]);
+            }
+        });
+    }
+
+    #[cfg(feature = "sqlite")]
+    #[bench]
+    fn bench_sqlite_set_get(b: &mut Bencher) {
+        let backend = SqliteBackend::in_memory().unwrap();
+        let mut kv = Kv::new(backend);
+        b.iter(|| {
+            for i in 0..1000u64 {
+                let k = key!["x", i];
+                kv.set(k.clone(), black_box(i)).unwrap();
+                let _ = kv.get::<u64>(&k).unwrap();
+            }
+        });
+    }
+}
